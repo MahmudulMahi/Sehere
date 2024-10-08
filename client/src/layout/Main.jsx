3c66cd4e466aced6c6cc9efda617d3254a17d38b
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-10-08 16:15:32 +0600

    setup firebase

--- client/src/layout/Main.jsx
+++ client/src/layout/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navber from '../components/Navber';
 
 const Main = () => {
   return (
     <div>
-      <h1>hhh</h1>
+      <Navber></Navber>
     </div>
   );
 };
