0c43de4ae55b1fe26c7d2521459785f8f88e1334
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-10-08 23:45:40 +0600

    add logo

--- client/src/components/Navber.jsx
+++ client/src/components/Navber.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../assets/images/logo.png'
 
 const Navber = () => {
   return (
@@ -6,7 +7,7 @@ const Navber = () => {
       <div className='navbar bg-base-100 shadow-sm container px-4 mx-auto'>
       <div className='flex-1'>
         <div className='flex gap-2 items-center'>
-          <img className='w-auto h-7' src='' alt='' />
+          <img className='w-auto h-7' src={logo} alt='' />
           <span className='font-bold'>SoloSphere</span>
         </div>
       </div>
