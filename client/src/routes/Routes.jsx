7c13e8dda46a3e60e05ddf4e2208e065ec953dae
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-10-08 23:58:12 +0600

    add login signin

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,11 +1,18 @@
 import {createBrowserRouter} from 'react-router-dom'
 import Main from '../layout/Main'
+import Home from '../pages/authinticaton/home/Home'
 
 const router=createBrowserRouter([
 
   {
     path:'/',
-    element:<Main></Main>
+    element:<Main></Main>,
+    children:[
+      {
+        index:true,
+        element:<Home></Home>
+      }
+    ]
   }
 ])
 
