ae24cf9ad78502207e934a4f8d2b342e223e71ea
Md Mahmudul Islam <33577043+MahmudulMahi@users.noreply.github.com>
2024-10-09 01:57:00 +0600

    add footer

--- client/src/layout/Main.jsx
+++ client/src/layout/Main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navber from '../components/Navber';
 import { Outlet } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 const Main = () => {
   return (
     <div>
       <Navber></Navber>
+      <div className='min-h-[calc(100vh-306px)]'>
       <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
--- client/src/pages/authinticaton/Login.jsx
+++ client/src/pages/authinticaton/Login.jsx
@@ -1,9 +1,42 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { Link, useNavigate, } from 'react-router-dom';
 import bgImg from '../../assets/images/login.jpg'
 import logo from '../../assets/images/logo.png'
+import { AuthContext } from '../../provider/AuthProvider';
+import toast from 'react-hot-toast';
 
 const Login = () => {
+  const navigate=useNavigate()
+
+  const {signIn,signInWithGoogle}=useContext(AuthContext)
+
+  const handleGoogleSignIn = async()=>{
+   try{
+    await signInWithGoogle()
+    toast.success('Sign in Successful')
+    navigate('/')
+   }catch(err){
+    console.log(err);
+    toast.error(err?.message)
+   }
+  }
+  const handleSignIn=async(e)=>{
+    e.preventDefault()
+    const form=e.target
+    const email=form.email.value
+    const pass=form.password.value
+    console.log(email,pass);
+    try{
+      const result=await signIn(email,pass)
+      console.log(result);
+      navigate('/')
+      toast.success('Sign in Successful')
+    }
+    catch(err){
+      console.log(err);
+      toast.error(err?.message)
+     }
+  }
   return (
     <>
       <div className='flex justify-center items-center min-h-[calc(100vh-306px)]'>
@@ -28,7 +61,7 @@ const Login = () => {
             Welcome back!
           </p>
 
-          <div className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
+          <div onClick={handleGoogleSignIn} className='flex cursor-pointer items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg   hover:bg-gray-50 '>
             <div className='px-4 py-2'>
               <svg className='w-6 h-6' viewBox='0 0 40 40'>
                 <path
@@ -64,7 +97,7 @@ const Login = () => {
 
             <span className='w-1/5 border-b dark:border-gray-400 lg:w-1/4'></span>
           </div>
-          <form>
+          <form onClick={handleSignIn}>
             <div className='mt-4'>
               <label
                 className='block mb-2 text-sm font-medium text-gray-600 '
